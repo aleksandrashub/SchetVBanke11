1d7c1a6dd11ff0e59531a79128a49de9877d477b
aleksandrashub <115869636+aleksandrashub@users.noreply.github.com>
2023-10-25 12:41:25 +0300

    Add files via upload

---
+++ SchetBanka/Class1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchetBanka
+{
+    public class SchetVBanke
+    {
+
+        private int nom;
+        private string name;
+        private float sum;
+
+        public void chooseSchet(int nomer, SchetVBanke schet)
+        { 
+        
+        
+        
+        }
+        public void choose(int otvet)
+        {
+            switch (otvet)
+            {
+                case 1:
+                    otk(this.nom, this.name, this.sum);
+                    break;
+                case 2:
+                   output();
+                    break;
+                case 3:
+                    dob(this.sum);
+                    break;
+                case 4:
+                   umen(this.sum);
+                    break;
+                case 5:
+                    obnul();
+                    break;
+             
+
+            }
+
+
+        }
+        public void choose(SchetVBanke schet, int chosen)
+        {
+            switch (chosen)
+            {
+                case 6:
+                    perenos(schet);
+
+                    break;
+
+
+            }
+
+
+        }
+
+
+        private void otk(int nom, string name, float sum)
+        {
+            Console.WriteLine("Введите ФИО: ");
+            this.name = Console.ReadLine();
+            Console.WriteLine("Введите номер счета: ");
+            this.nom = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите сумму, которую хотите перевести на счет: ");
+            this.sum = Convert.ToInt32(Console.ReadLine());
+            if (this.sum < 0)
+            {
+                Console.WriteLine("Баланс не может быть отрицательным. Баланс 0");
+                this.sum = 0;   
+            }
+
+        }
+
+        private void output()
+        {
+            Console.WriteLine("Информация о счете: \n Имя, на которое зарегистрирован счет: " + this.name + "\n Номер счета: " + this.nom + "\n Сумма на счету: " + this.sum);
+
+        }
+
+        private void dob(float sum)
+        {
+            Console.WriteLine("Введите сумму, которую хотите положить на счет: ");
+            float top = float.Parse(Console.ReadLine());
+            this.sum += top;
+            Console.WriteLine("Новая сумма на вашем счету: " + this.sum);
+
+        }
+
+
+
+        private void umen(float sum)
+        {
+            Console.WriteLine("Введите сумму, которую хотите снять со счета: ");
+            float umen = float.Parse(Console.ReadLine());
+            if (umen > this.sum)
+            {
+
+                Console.WriteLine("Вы хотите снять больше, чем имеется, баланс 0");
+                this.sum = 0;
+
+            }
+            else
+            {
+                this.sum -= umen;
+                Console.WriteLine("Новая сумма на вашем счету: " + this.sum);
+
+
+            }
+            
+        }
+
+        private void obnul()
+        {
+            this.sum = 0;
+            Console.WriteLine("Ваш счет: 0");
+        
+        
+        }
+
+        private void perenos(SchetVBanke schet)
+        {
+           // Console.WriteLine(" какого счета хотите перевести? Введите его номер nom" ); 
+           // int otvet = Convert.ToInt32(Console.ReadLine());
+           
+          //  if (otvet == this.nom)
+           // {
+                Console.WriteLine("Какую сумму?");
+                float summa = float.Parse(Console.ReadLine());
+            if (summa > this.sum)
+            {
+
+                Console.WriteLine("Сумма превышает баланс");
+            }
+            else 
+            {
+            schet.sum += summa;
+                this.sum -= summa;
+                Console.WriteLine("Новая сумма счета " + this.nom + " = " + this.sum + "\n Новая сумма счета " + schet.nom + " = " + schet.sum);
+            
+            }
+                
+
+          //  }
+         /*   else if (otvet == schet.nom)
+            {
+                Console.WriteLine("Какую сумму?");
+                float summa = float.Parse(Console.ReadLine());
+                this.sum += summa;
+                schet.sum -= summa;
+                Console.WriteLine("Новая сумма счета " + this.nom + " = " + this.sum + "\n Новая сумма счета " + schet.nom + " = " + schet.sum);
+
+            }
+            else
+            { 
+            Console.WriteLine("Такого счета у вас нет");
+            
+            }
+         */
+        
+        }
+
+        
+
+
+
+    }
+}
---
+++ SchetBanka/Program.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using SchetBanka;
+
+namespace SchetBanka
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<SchetVBanke> scheta = new List<SchetVBanke>();
+
+            //   for (int i = 0; i < scheta.Count; i++)
+            //   {
+
+            //      scheta[i] = new SchetVBanke();
+
+
+            //  }
+            string otvet1;
+
+            int otvetSchet = 1;
+            //  SchetVBanke schet = new SchetVBanke();
+            //  scheta.Add(schet);
+            int otvetMenu = 1;
+            int nomer;
+            while (otvetSchet == 0 || otvetSchet == 1 )
+            {
+                Console.WriteLine("Введите 1, чтобы создать новый счет или введите 0, чтобы перейти в меню уже существующего");
+                otvetSchet = Convert.ToInt32(Console.ReadLine());
+
+                if (otvetSchet == 1)
+                {
+                    SchetVBanke schet = new SchetVBanke();
+                    scheta.Add(schet);
+                    otvetSchet = 0;
+                    nomer = scheta.IndexOf(schet) + 1;
+
+                }
+
+                else if (otvetSchet == 0)
+                {
+                    Console.WriteLine("У вас " + scheta.Count + " счетов ");
+                    Console.WriteLine("Введите номер (1, 2...) счета, в меню которого хотите перейти");
+                    nomer = Convert.ToInt32(Console.ReadLine());
+                }
+                else
+                {
+                    break;
+                }
+
+                for (int i = 0; i < scheta.Count; i++)
+                {
+                    if (nomer == i + 1)
+                    {
+
+                        otvetMenu = 1;
+                        
+                        while (otvetMenu >= 1 && otvetMenu < 7)
+                        {
+
+                            Console.WriteLine("Меню счета " + (i + 1) + ": \n 1 - заполнить информацию счета \n 2 - показать информацию о счете \n 3 - положить на счет \n 4 - снять со счета \n 5 - взять всю сумму \n 6 - перенести сумму с одного счета на другой" +
+                                  " \n 7 - перейти в меню другого счета или создать новый");
+                            otvetMenu = Convert.ToInt32(Console.ReadLine());
+                            switch (otvetMenu)
+                            {
+                                case 1:
+                                case 2:
+                                case 3:
+                                case 4:
+                                case 5:
+                                    scheta[i].choose(otvetMenu);
+                                    break;
+                                case 6:
+                                    Console.WriteLine("Введите номер счета (1, 2...), на который хотите перевести сумму с этого");
+                                    nomer = Convert.ToInt32(Console.ReadLine());
+                                    bool check = false;
+                                    for (int j = 0; j < scheta.Count; j++)
+                                    {
+                                        if (nomer == j + 1)
+                                        {
+                                            check = true;
+                                            scheta[i].choose(scheta[j], otvetMenu);
+                                            break;
+
+                                        }
+
+                                    }
+                                    if (check == false)
+                                    {
+                                        Console.WriteLine("Такого счета нет");
+                                        break;
+                                    }
+                                    break;
+                                case 7:
+                                    break;
+                                    // Console.WriteLine("У вас " + scheta.Count + " счетов ");
+                                    // Console.WriteLine("Введите номер (1, 2...) счета, в меню которого хотите перейти");
+                                    // nomer = Convert.ToInt32(Console.ReadLine());
+                                    break;
+
+                                    //  break;
+
+
+                            }
+
+                            //  Console.WriteLine("Хотите вернуться в меню счета №" + otvetSchet + "?");
+                            //   otvet1 = Console.ReadLine();
+                            //  if (otvet1 == "нет")
+                            //  {
+                            //      break;
+                            // Console.WriteLine(""); 
+                            //  }
+
+                        //    if (otvetMenu == 7)
+                         //   {
+
+                        //        break;
+                         //   }
+                        }
+                       
+                    }
+                    else
+                    {
+                        continue;
+                    }
+
+                 //   if (otvetMenu == 7)
+                  //  {
+
+                  //      break;
+                  //  }
+
+
+                }
+
+
+
+                //    int otvetMenu = 1;
+                //  otvet1 = "да";
+                /*      while (otvetMenu >= 1 && otvetMenu < 7)
+                      {
+
+                          Console.WriteLine("Меню счета " + otvetSchet + ": \n 1 - заполнить информацию счета \n 2 - показать информацию о счете \n 3 - положить на счет \n 4 - снять со счета \n 5 - взять всю сумму \n 6 - перенести сумму с одного счета на другой" +
+                              " \n 7 - перейти в меню другого счета или создать новый");
+                          otvetMenu = Convert.ToInt32(Console.ReadLine());
+                          switch (otvetMenu)
+                          {
+                              case 1:
+                              case 2:
+                              case 3:
+                              case 4:
+                              case 5:
+                                  scheta[otvetSchet - 1].choose(otvetMenu);
+                                  break;
+                              case 6:
+                                  switch (otvetSchet - 1)
+                                  {
+                                      case 0:
+                                          scheta[0].choose(scheta[1], otvetMenu);
+                                          break;
+                                      case 1:
+                                          scheta[1].choose(scheta[0], otvetMenu);
+                                          break;
+                                  }
+                                  break;
+                              case 7:
+                                  // Console.WriteLine("");
+                                  break;
+                              default: break;
+
+                          }
+                      }
+
+
+
+                  if (otvetSchet == 0)
+                  {
+                      Console.WriteLine("У вас " + scheta.Count + " счетов ");
+                      Console.WriteLine("Введите номер (1, 2...) счета, в меню которого хотите перейти");
+                      int nomer = Convert.ToInt32(Console.ReadLine());
+                      for (int i = 0; i < scheta.Count; i++)
+                      {
+                          if (nomer == i + 1)
+                          {
+                              otvet1 = "да";
+                              while (otvet1 == "да")
+                              {
+
+                                  Console.WriteLine("Меню счета " + i + ": \n 1 - открыть счет \n 2 - показать информацию о счете \n 3 - положить на счет \n 4 - снять со счета \n 5 - взять всю сумму \n 6 - перенести сумму с одного счета на другой");
+                                  int otvetMenu = Convert.ToInt32(Console.ReadLine());
+                                  switch (otvetMenu)
+                                  {
+                                      case 1:
+                                      case 2:
+                                      case 3:
+                                      case 4:
+                                      case 5:
+                                          scheta[i].choose(otvetMenu);
+                                          break;
+                                      case 6:
+                                          Console.WriteLine("Введите номер счета, на который хотите перевести сумму с этого");
+                                           nomer = Convert.ToInt32(Console.ReadLine());
+                                          for (int j = 0; j < scheta.Count; j++)
+                                          {
+                                              if (nomer == j + 1)
+                                              {
+                                                  scheta[i].choose(scheta[j], otvetMenu);
+                                                  break;
+
+                                              }
+                                          }
+                                              break;
+
+
+                                  }
+
+                                  Console.WriteLine("Хотите вернуться в меню счета №" + otvetSchet + "?");
+                                  otvet1 = Console.ReadLine();
+                                  if (otvet1 == "нет")
+                                  {
+                                      break;
+                                      // Console.WriteLine(""); 
+                                  }
+
+
+                              }
+
+                          }
+                          else
+                          {
+                              continue;
+                          }
+
+
+
+
+                      }
+
+                  }
+
+
+
+              }
+
+              */
+
+
+
+
+
+
+
+
+
+
+               // Console.ReadLine();
+
+
+            }
+        }
+    }
+}
